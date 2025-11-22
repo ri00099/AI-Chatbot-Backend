54904d9e5ca876d42775ab6e9c4311c4deff48a3
Ritesh Sharma <ri00099g@gmail.com>
2025-11-23 01:59:51 +0530

    update#2

--- server.js
+++ server.js
@@ -70,6 +70,8 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
